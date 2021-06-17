c4582398cd0f5f2ee92d89d769cfe59ba9b66e55
luisforgu <luis.forero@carvajal.com>
2021-06-16 23:56:35 -0500

    Elimar tweets

---
+++ twittor/bd/borroTweet.go
@@ -0,0 +1,29 @@
+package bd
+
+import (
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+/*BorroTweet borra un tweet determinado */
+func BorroTweet(ID string, UserID string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*15)
+	defer cancel()
+
+	db := MongoCN.Database("twittor")
+	col := db.Collection("tweet")
+
+	objID, _ := primitive.ObjectIDFromHex(ID)
+
+	condicion := bson.M{
+		"_id":    objID,
+		"userid": UserID,
+	}
+
+	//Condicion Mongo para eliminar un registro de tweet
+	_, err := col.DeleteOne(ctx, condicion)
+	return err
+}
